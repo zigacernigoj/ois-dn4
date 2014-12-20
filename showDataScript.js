59d2ae052d98a78154670a6581a4d75caccd4805
Ziga <zc6857@student.uni-lj.si>
2014-12-20 16:08:26 +0100

    10g

--- showDataScript.js
+++ showDataScript.js
@@ -19,48 +19,50 @@ function getData(){
 	$.getJSON("testna1.json", function(data){
 	  //alert("deluje!");
 	  var results = "<table class='table table-striped table-hover'><tr><th>code</th><th>main</th><th>sub1</th><th>sub2</th><th>sub3</th><th>deaths (2012)</th></tr>";
+	  var tabledata2="<table class='table table-striped table-hover'><tr>"
+	  
 	  var j=0, k=0, l=0, m=0;
 	  for (var i in data){
 		results += "<tr><td>" + data[i].code + "</td><td>" + data[i].main +"</td><td>" + data[i].sub1 + "</td><td>" + data[i].sub2 +"</td><td>" + data[i].sub3 +"</td><td>" +  data[i].Deaths2012 +"</td></tr>";
+		tabledata2+="<tr>";
 		if(data[i].code>0 && data[i].main && data[i].main.trim().length>3){
-			mainCat[j]= j + " " + data[i].main + "</br>";
+			mainCat[j]= j + " " + data[i].main;
 			//$("#data2").append(data[i].code +": "+ data[i].main+ " ; j=" + j + "</br>");
+			tabledata2+="<td>"+mainCat+"</td>";
 			j++;
 		}
 		
 		if(data[i].code>0 && data[i].sub1 && data[i].sub1.trim().length>3){
-			sub1Cat[k]= j + "." + k + " " + data[i].sub1 + "</br>";
+			sub1Cat[k]= j + "." + k + " " + data[i].sub1;
 			//$("#data2").append(data[i].code +": "+ data[i].sub1+ " ; k=" + k + "</br>");
+			tabledata2+="<td>"+sub1Cat+"</td>";
 			k++;
 		}
 		
 		if(data[i].code>0 && data[i].sub2 && data[i].sub2.trim().length>3){
-			sub2Cat[l]= j + "." + k + "." + l + data[i].sub2 + "</br>";
+			sub2Cat[l]= j + "." + k + "." + l + data[i].sub2;
 			//$("#data2").append(data[i].code +": "+ data[i].sub2+ " ; l=" + k + "</br>");
+			tabledata2+="<td>"+sub2Cat+"</td>";
 			l++;
 		}
 		
 		if(data[i].code>0 && data[i].sub3 && data[i].sub3.trim().length>3){
-			sub3Cat[m]= j + "." + k + "." + l + "." + m + data[i].sub3 + "</br>";
+			sub3Cat[m]= j + "." + k + "." + l + "." + m + data[i].sub3;
 			//$("#data2").append(data[i].code +": "+ data[i].sub3+ " ; m=" + k + "</br>");
+			tabledata2+="<td>"+sub3Cat+"</td>";
 			m++;
 		}
 		
-		
-		
+		tabledata2+="</tr>";
 	  }
+	  
+	  
 	   results += "</table>";
+	   tabledata2 += "</table>";
+	   
 	  $("#data1").append(results);
 	  //$("#data1").append(JSON.stringify(data));
 	  
-	  var tabledata2="<table class='table table-striped table-hover'><tr>"
-						+"<td>"+mainCat+"</td>"
-						+"<td>"+sub1Cat+"</td>"
-						+"<td>"+sub2Cat+"</td>"
-						+"<td>"+sub3Cat+"</td>"
-						+"</tr></table>";
-
-
 		$("#data2").append(tabledata2);
 	});	
 		
