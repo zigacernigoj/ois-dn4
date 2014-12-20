baea77c3d1f4d51e283e783ff7e2a307a9e17727
Ziga <zc6857@student.uni-lj.si>
2014-12-20 02:18:05 +0100

    9

--- showDataScript.js
+++ showDataScript.js
@@ -15,9 +15,9 @@ $.ajax({
 		
 	$.getJSON("testna1.json", function(data){
 	  //alert("deluje!");
-	  var results = "<table class='table table-striped table-hover'><tr><th>T1</th><th class='text-right'>T2</th></tr>";
+	  var results = "<table class='table table-striped table-hover'><tr><th>code</th><th>main</th><th>sub1</th><th>sub2</th><th>sub3</th><th>deaths (2012)</th></tr>";
 	  for (var i in data){
-		results += "<tr><td>" + data[i].code + "</td><td class='text-right'>" + data[i].Deaths2012 +"</td></tr>";
+		results += "<tr><td>" + data[i].code + "</td><td>" + "</td><td>" + data[i].main +"</td><td>" + data[i].sub1 + "</td><td>" + data[i].sub2 +"</td><td>" + data[i].sub3 +"</td><td>" +  data[i].Deaths2012 +"</td></tr>";
 	  }
 	   results += "</table>";
 	  $("#data1").append(results);
