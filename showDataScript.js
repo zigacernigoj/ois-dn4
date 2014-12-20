ad6d34181d2c44fef4cadba09246851acf284b2e
Ziga <zc6857@student.uni-lj.si>
2014-12-20 02:29:28 +0100

    9delej

--- showDataScript.js
+++ showDataScript.js
@@ -1,16 +1,18 @@
 function getData(){
+
 /*
-$.ajax({
-			url: 'testna1.json',
-	    	success: function (data) {
-				$("#data").html(JSON.stringify(data));
-				console.log(data);
-			},
-			error: function(err) {
-				$("#data").html(JSON.parse(err.responseText).userMessage);
-				console.log(JSON.parse(err.responseText).userMessage);
-			}
-		});*/
+	$.ajax({
+		url: 'testna1.json',
+		success: function (data) {
+			$("#data").html(JSON.stringify(data));
+			console.log(data);
+		},
+		error: function(err) {
+			$("#data").html(JSON.parse(err.responseText).userMessage);
+			console.log(JSON.parse(err.responseText).userMessage);
+		}
+	});
+*/
 		
 		
 	$.getJSON("testna1.json", function(data){
