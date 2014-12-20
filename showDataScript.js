886f7bc4df1fe1b3e954c65b8938ac6041b1e219
Ziga <zc6857@student.uni-lj.si>
2014-12-20 14:18:09 +0100

    10

--- showDataScript.js
+++ showDataScript.js
@@ -13,17 +13,28 @@ function getData(){
 		}
 	});
 */
-		
+	var mainCat, sub1Cat, sub2Cat, sub3Cat;
+	
 		
 	$.getJSON("testna1.json", function(data){
 	  //alert("deluje!");
 	  var results = "<table class='table table-striped table-hover'><tr><th>code</th><th>main</th><th>sub1</th><th>sub2</th><th>sub3</th><th>deaths (2012)</th></tr>";
+	  var j = 0;
 	  for (var i in data){
 		results += "<tr><td>" + data[i].code + "</td><td>" + data[i].main +"</td><td>" + data[i].sub1 + "</td><td>" + data[i].sub2 +"</td><td>" + data[i].sub3 +"</td><td>" +  data[i].Deaths2012 +"</td></tr>";
+		if(data[i].main.trim().length>2 && data[i].code>0){
+			mainCat[j]=data[i].main.trim();
+			j++;
+		}
+		
+		
+		
+		
 	  }
 	   results += "</table>";
 	  $("#data1").append(results);
 	  $("#data1").append(JSON.stringify(data));
+	   $("#data2").append(mainCat);
 	});	
 		
 	//$("#data2").html(JSON.stringify(data));	
