15721ef4a6eb0317ba0d5f9e06eb6f279efab34a
Ziga <zc6857@student.uni-lj.si>
2014-12-20 15:17:37 +0100

    10f1

--- showDataScript.js
+++ showDataScript.js
@@ -22,25 +22,25 @@ function getData(){
 	  var j=0, k=0, l=0, m=0;
 	  for (var i in data){
 		results += "<tr><td>" + data[i].code + "</td><td>" + data[i].main +"</td><td>" + data[i].sub1 + "</td><td>" + data[i].sub2 +"</td><td>" + data[i].sub3 +"</td><td>" +  data[i].Deaths2012 +"</td></tr>";
-		if(data[i].main && data[i].main.trim().length>3){
-			mainCat[j]=JSON.stringify(data[i].main);
+		if(data[i].code>0 && data[i].main && data[i].main.trim().length>3){
+			mainCat[j]=data[i].main;
 			//$("#data2").append(data[i].code +": "+ data[i].main+ " ; j=" + j + "</br>");
 			j++;
 		}
 		
-		if(data[i].sub1 && data[i].sub1.trim().length>3){
+		if(data[i].code>0 && data[i].sub1 && data[i].sub1.trim().length>3){
 			sub1Cat[k]=JSON.stringify(data[i].sub1);
 			//$("#data2").append(data[i].code +": "+ data[i].sub1+ " ; k=" + k + "</br>");
 			k++;
 		}
 		
-		if(data[i].sub2 && data[i].sub2.trim().length>3){
+		if(data[i].code>0 && data[i].sub2 && data[i].sub2.trim().length>3){
 			sub2Cat[l]=JSON.stringify(data[i].sub2);
 			//$("#data2").append(data[i].code +": "+ data[i].sub2+ " ; l=" + k + "</br>");
 			l++;
 		}
 		
-		if(data[i].sub3 && data[i].sub3.trim().length>3){
+		if(data[i].code>0 && data[i].sub3 && data[i].sub3.trim().length>3){
 			sub3Cat[m]=JSON.stringify(data[i].sub3);
 			//$("#data2").append(data[i].code +": "+ data[i].sub3+ " ; m=" + k + "</br>");
 			m++;
