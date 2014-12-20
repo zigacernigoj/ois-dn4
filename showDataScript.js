faf99145dc090e6097075cdedc8c7051d38d804b
Ziga <zc6857@student.uni-lj.si>
2014-12-20 14:40:32 +0100

    10a3a

--- showDataScript.js
+++ showDataScript.js
@@ -23,8 +23,8 @@ function getData(){
 	  for (var i in data){
 		results += "<tr><td>" + data[i].code + "</td><td>" + data[i].main +"</td><td>" + data[i].sub1 + "</td><td>" + data[i].sub2 +"</td><td>" + data[i].sub3 +"</td><td>" +  data[i].Deaths2012 +"</td></tr>";
 		if(data[i].main){
-			mainCat[j]=JSON.stringify(data[i].main);
-			$("#data2").append(mainCat[j]);
+			//mainCat[j]=JSON.stringify(data[i].main);
+			$("#data2").append(data[i].main);
 			j++;
 		}
 		
