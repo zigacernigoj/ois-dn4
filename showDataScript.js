aeac9a6c7bc08fc03b280515cbd2abcc18236b82
Ziga <zc6857@student.uni-lj.si>
2014-12-20 14:24:38 +0100

    10a1

--- showDataScript.js
+++ showDataScript.js
@@ -22,8 +22,8 @@ function getData(){
 	  var j = 0;
 	  for (var i in data){
 		results += "<tr><td>" + data[i].code + "</td><td>" + data[i].main +"</td><td>" + data[i].sub1 + "</td><td>" + data[i].sub2 +"</td><td>" + data[i].sub3 +"</td><td>" +  data[i].Deaths2012 +"</td></tr>";
-		if(String(data[i].main.trim()).length>2 && data[i].code>0){
-			mainCat[j]=data[i].main.trim();
+		if(String(data[i].main).trim().length>2 && data[i].code>0){
+			mainCat[j]=String(data[i].main).trim();
 			j++;
 		}
 		
