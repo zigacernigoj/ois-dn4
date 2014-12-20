1fe9595f0704dab6b3544c54d273e2fc15100067
Ziga <zc6857@student.uni-lj.si>
2014-12-20 17:57:22 +0100

    11f

--- showDataScript.js
+++ showDataScript.js
@@ -24,8 +24,13 @@ function getData(){
 	  var results = "<table class='table table-striped table-hover'><tr><th>code</th><th>main</th><th>sub1</th><th>sub2</th><th>sub3</th><th>deaths (2012)</th></tr>";
 	  var tabledata2="<table class='table table-striped table-hover'>";
 	
-	  var select="<option value=''></option>";
+	  var selectMain="<option value=''></option>";
+	  var selectS1="<option value=''></option>";
+	  var selectS2="<option value=''></option>";
+	  var selectS3="<option value=''></option>";
+	  
 
+	  
 	  var j=0, k=0, l=0, m=0;
 	  for (var i in data){
 		results += "<tr><td>" + data[i].code + "</td><td>" + data[i].main +"</td><td>" + data[i].sub1 + "</td><td>" + data[i].sub2 +"</td><td>" + data[i].sub3 +"</td><td>" +  data[i].Deaths2012 +"</td></tr>";
@@ -35,9 +40,7 @@ function getData(){
 			mainCat[j]= j + " " + data[i].main;
 			//$("#data2").append(data[i].code +": "+ data[i].main+ " ; j=" + j + "</br>");
 			tabledata2+="<td>"+mainCat[j]+"</td>";
-			select+="<option value='"+mainCat[j]+"'>"+mainCat[j]+"</option>";
-			
-			
+			selectMain+="<option value='"+mainCat[j]+"'>"+mainCat[j]+"</option>";
 			k=0;
 		}
 		
@@ -46,22 +49,25 @@ function getData(){
 			sub1Cat[k]= j + "." + k + " " + data[i].sub1;
 			//$("#data2").append(data[i].code +": "+ data[i].sub1+ " ; k=" + k + "</br>");
 			tabledata2+="<td>"+sub1Cat[k]+"</td>";
+			selectS1+="<option value='"+sub1Cat[k]+"'>"+sub1Cat[k]+"</option>";
 			l=0;
 		}
 		
 		if(data[i].code>0 && data[i].sub2 && data[i].sub2.trim().length>3){
 			l++;
-			sub2Cat[l]= j + "." + k + "." + l + data[i].sub2;
+			sub2Cat[l]= j + "." + k + "." + l + " " + data[i].sub2;
 			//$("#data2").append(data[i].code +": "+ data[i].sub2+ " ; l=" + k + "</br>");
 			tabledata2+="<td>"+sub2Cat[l]+"</td>";
+			selectS2+="<option value='"+sub2Cat[l]+"'>"+sub2Cat[l]+"</option>";
 			m=0;
 		}
 		
 		if(data[i].code>0 && data[i].sub3 && data[i].sub3.trim().length>3){
 			m++;
-			sub3Cat[m]= j + "." + k + "." + l + "." + m + data[i].sub3;
+			sub3Cat[m]= j + "." + k + "." + l + "." + m + " " + data[i].sub3;
 			//$("#data2").append(data[i].code +": "+ data[i].sub3+ " ; m=" + k + "</br>");
 			tabledata2+="<td>"+sub3Cat[m]+"</td>";
+			selectS3+="<option value='"+sub3Cat[m]+"'>"+sub3Cat[l]+"</option>";
 		}
 		
 		tabledata2+="</tr>";
@@ -75,7 +81,10 @@ function getData(){
 	  //$("#data1").append(JSON.stringify(data));
 	  
 		$("#data2").append(tabledata2);
-		$("#chooseMain").append(select);
+		$("#chooseMain").append(selectMain);
+		$("#chooseS1").append(selectS1);
+		$("#chooseS2").append(selectS2);
+		$("#chooseS3").append(selectS3);
 	});	
 		
 	//$("#data2").html(JSON.stringify(data));	
