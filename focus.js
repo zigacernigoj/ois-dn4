365e8f18feff9ba6becaa1707fd6cab1b7b8411f
Ziga <zc6857@student.uni-lj.si>
2014-12-22 00:55:07 +0100

    oneExtra

--- focus.js
+++ focus.js
@@ -1,36 +1,44 @@
 function focusGuide() {
 	if(document.getElementById("guideShow").style.display == 'none'){
 		document.getElementById("guideShow").style.display = 'block';
+		$("#hg").append("<span class='label label-default fade-in'>opened</span>");
 	}
 	else if(document.getElementById("guideShow").style.display == 'block'){
-		document.getElementById("guideShow").style.display = 'none';		
+		document.getElementById("guideShow").style.display = 'none';
+		$("#hg").append("");
 	}
 }
 
 function focusUser() {
 	if(document.getElementById("userShow").style.display == 'none'){
 		document.getElementById("userShow").style.display = 'block';
+		$("#hu").append("<span class='label label-default fade-in'>opened</span>");
 	}
 	else if(document.getElementById("userShow").style.display == 'block'){
-		document.getElementById("userShow").style.display = 'none';		
+		document.getElementById("userShow").style.display = 'none';	
+		$("#hu").append("");
 	}
 }
 
 function focusDiagnosis() {
 	if(document.getElementById("diagnosisShow").style.display == 'none'){
 		document.getElementById("diagnosisShow").style.display = 'block';
+			$("#hd").append("<span class='label label-default fade-in'>opened</span>");
 	}
 	else if(document.getElementById("diagnosisShow").style.display == 'block'){
-		document.getElementById("diagnosisShow").style.display = 'none';		
+		document.getElementById("diagnosisShow").style.display = 'none';
+		$("#hd").append("");		
 	}
 }
 
 function focusShowDiag() {
 	if(document.getElementById("dataShow").style.display == 'none'){
 		document.getElementById("dataShow").style.display = 'block';
+			$("#hs").append("<span class='label label-default fade-in'>opened</span>");
 	}
 	else if(document.getElementById("dataShow").style.display == 'block'){
 		document.getElementById("dataShow").style.display = 'none';		
+		$("#hs").append("");
 	}
 }
 
